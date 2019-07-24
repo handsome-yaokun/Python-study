de51e22521bd20189e26fb1ef7eb901a127f07aa
handsome-yaokun <48851792+handsome-yaokun@users.noreply.github.com>
2019-07-24 12:20:13 +0800

    Add files via upload

---
+++ The first game.py
@@ -0,0 +1,23 @@
+import random
+answer = random.randint(1, 10)
+times = 3
+print('----------made by yaokun group----------')
+temp = input('Please guess the number that 1-10:')
+while temp.isdigit():
+    temp = input('please enter a number that from 1 to 10')
+guess = int(temp)
+while times != 0:
+    if guess == answer:
+        print('What amazing!You just guessed the right number!')
+    else:
+
+
+
+
+
+
+
+
+
+
+
---
+++ hello.py
@@ -0,0 +1,3 @@
+name = input('Please enter your name,and we will say hello to you:')
+print('hello!', name,)
+print('Would you like something different?', name, )
