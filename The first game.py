92886904787e7b69f0a94453154b9e605f4c915d
handsome-yaokun <48851792+handsome-yaokun@users.noreply.github.com>
2019-07-24 12:22:49 +0800

    Update The first game.py

--- The first game.py
+++ The first game.py
@@ -1,7 +1,7 @@
 import random
 answer = random.randint(1, 10)
 times = 3
-print('----------made by yaokun group----------')
+print('----------made by yaokun----------')
 temp = input('Please guess the number that 1-10:')
 while temp.isdigit():
     temp = input('please enter a number that from 1 to 10')
